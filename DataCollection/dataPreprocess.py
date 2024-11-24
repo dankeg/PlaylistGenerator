1e05a9058d519bab85732e1d139749d8712bb7f8
yuvi07k <116238605+yuvi07k@users.noreply.github.com>
2024-11-24 04:30:18 -0500

    Update dataPreprocess.py

--- DataCollection/dataPreprocess.py
+++ DataCollection/dataPreprocess.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import re
+from langdetect import detect
 import nltk
 from sklearn.preprocessing import MinMaxScaler
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-from sklearn.preprocessing import LabelEncoder
 nltk.download('stopwords')
 
 # Load dataset
@@ -13,32 +13,41 @@ data = pd.read_csv('../Datasets/spotify_songs.csv')
 
 # Select relevant columns
 columns_to_keep = [
-    'track_id', 'track_name', 'track_artist', 'lyrics',
-    'track_popularity', 'playlist_genre', 'playlist_subgenre', 'danceability',
+    'id', 'name', 'artists', 'danceability',
     'energy', 'loudness', 'mode', 'speechiness', 'acousticness',
-    'instrumentalness', 'liveness', 'valence', 'tempo', 'language'
+    'instrumentalness', 'liveness', 'valence', 'tempo', 'lyrics'
 ]
+
 data = data[columns_to_keep]
+
+data['artists'] = data['artists'].astype(str)
+
+data = data.drop_duplicates()
 
 # Drop rows with missing values
 data = data.dropna()
 
+data = data.sample(n=75000, random_state=42)
+
+# Function to detect language of the lyrics
+def is_english(text):
+    try:
+        return detect(text) == 'en'
+    except:
+        return False
+
+# Filter English songs
+data = data[data['lyrics'].apply(is_english)]
+
 # Normalize numerical columns
 numerical_columns = [
-    'track_popularity', 'danceability', 'energy', 'loudness', 
+    'danceability', 'energy', 'loudness', 
     'speechiness', 'acousticness', 'instrumentalness', 
     'liveness', 'valence', 'tempo'
 ]
+
 scaler = MinMaxScaler()
 data[numerical_columns] = scaler.fit_transform(data[numerical_columns])
-
-
-data['lyric_length'] = data['lyrics'].apply(lambda x: len(x.split()))
-data['sentence_count'] = data['lyrics'].apply(lambda x: len(re.split(r'[.!?]', x)))
-
-# Analyze genre distribution (imbalance check)
-genre_counts = data['playlist_genre'].value_counts()
-print("Genre Distribution:\n", genre_counts)
 
 # Text cleaning, stopwords removal
 stop_words = set(stopwords.words('english'))
@@ -49,27 +58,14 @@ def advanced_preprocess_lyrics(lyrics):
     tokens = [word for word in tokens if word not in stop_words]  # Remove stopwords
     return ' '.join(tokens)
 
-data['cleaned_lyrics'] = data['lyrics'].apply(advanced_preprocess_lyrics)
-
-label_encoder_genre = LabelEncoder()
-label_encoder_subgenre = LabelEncoder()
-
-data['genre_encoded'] = label_encoder_genre.fit_transform(data['playlist_genre'])
-data['subgenre_encoded'] = label_encoder_subgenre.fit_transform(data['playlist_subgenre'])
-
-# Save mappings for later interpretation
-genre_mapping = dict(zip(label_encoder_genre.classes_, label_encoder_genre.transform(label_encoder_genre.classes_)))
-subgenre_mapping = dict(zip(label_encoder_subgenre.classes_, label_encoder_subgenre.transform(label_encoder_subgenre.classes_)))
-
-print("Genre Mapping:", genre_mapping)
-print("Subgenre Mapping:", subgenre_mapping)
+data['lyrics'] = data['lyrics'].apply(advanced_preprocess_lyrics)
+data['artists'] = data['artists'].apply(advanced_preprocess_lyrics)
 
 # Save the cleaned data
 data.to_csv('../Datasets/cleaned_spotify_songs.csv', index=False)
 
 # List of words to exclude from the WordCloud
-exclude_list = []
-
+exclude_list = ["not a good word list to show"]
 
 def remove_excluded_words(lyrics, exclude_list):
     words = lyrics.split()
@@ -78,7 +74,7 @@ def remove_excluded_words(lyrics, exclude_list):
 
 # Combine all lyrics, excluding words from the exclude list
 filtered_lyrics = ' '.join(
-    data['cleaned_lyrics'].apply(lambda x: remove_excluded_words(x, exclude_list))
+    data['lyrics'].apply(lambda x: remove_excluded_words(x, exclude_list))
 )
 
 # Generate the WordCloud with filtered lyrics
