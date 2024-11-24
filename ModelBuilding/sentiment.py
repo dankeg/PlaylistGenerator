a395e39503acbf8d3ca1f1349c10e72c31d88e4f
yuvi07k <116238605+yuvi07k@users.noreply.github.com>
2024-11-24 08:42:29 -0500

    Update sentiment.py

--- ModelBuilding/sentiment.py
+++ ModelBuilding/sentiment.py
@@ -3,10 +3,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import matplotlib.pyplot as plt
 import seaborn as sns
-from wordcloud import WordCloud
 import pandas as pd
 
-
+# Load the cleaned dataset
 data = pd.read_csv('../Datasets/cleaned_spotify_songs.csv')
 
 # Ensure the cleaned_lyrics column has no NaN values or non-string types
@@ -32,8 +31,29 @@ def categorize_emotion(sentiment_score):
 
 data['emotion'] = data['sentiment_score'].apply(categorize_emotion)
 
-# Vectorize the lyrics
-vectorizer = CountVectorizer(max_features=1000, stop_words='english')  
+custom_stopwords = set([
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", 
+    "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", 
+    "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", 
+    "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", 
+    "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", 
+    "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", 
+    "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", 
+    "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", 
+    "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", 
+    "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", 
+    "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", 
+    "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", 
+    "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", 
+    "yourselves", "oh", "yeah", "nah", "woah", "hey", "ah", "uh", "la", "ooh", "doo", "ba", "da", "hmm", "whoa", 
+    "sha", "ye", "na-na", "ha", "dee", "yo", "ay", "baby", "girl", "boy", "hey", "hi", "bye", "ha", "ha-ha", "na-na-na", 
+    "sha-la-la", "hmm-mm", "ah-ah", "oh-oh", "da-da", "uh-oh", "yeah-yeah", "do-do", "woo", "y'all", "ain't", 
+    "gotta", "wanna", "gonna", "lemme", "yeah-yeah-yeah", "uh-huh", "uh-uh", "woah-woah"
+])
+
+custom_stopwords_list = list(custom_stopwords)
+# Vectorize the lyrics using custom stopwords
+vectorizer = CountVectorizer(max_features=1000, stop_words=custom_stopwords_list)
 lyrics_matrix = vectorizer.fit_transform(data['lyrics'])
 
 # LDA Model
