78447891ad47e9faf6b0758a946bdee836dc991a
yuvi07k <116238605+yuvi07k@users.noreply.github.com>
2024-11-24 08:41:57 -0500

    Update dataPreprocess.py

--- DataCollection/dataPreprocess.py
+++ DataCollection/dataPreprocess.py
@@ -1,13 +1,7 @@
 import pandas as pd
 import re
 from langdetect import detect
-import nltk
 from sklearn.preprocessing import MinMaxScaler
-from nltk.corpus import stopwords
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-nltk.download('stopwords')
-
 # Load dataset
 data = pd.read_csv('../Datasets/spotify_songs.csv')
 
@@ -49,13 +43,32 @@ numerical_columns = [
 scaler = MinMaxScaler()
 data[numerical_columns] = scaler.fit_transform(data[numerical_columns])
 
-# Text cleaning, stopwords removal
-stop_words = set(stopwords.words('english'))
+custom_stopwords = set([
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", 
+    "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", 
+    "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", 
+    "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", 
+    "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", 
+    "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", 
+    "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", 
+    "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", 
+    "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", 
+    "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", 
+    "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", 
+    "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", 
+    "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", 
+    "yourselves", "oh", "yeah", "nah", "woah", "hey", "ah", "uh", "la", "ooh", "doo", "ba", "da", "hmm", "whoa", 
+    "sha", "ye", "na-na", "ha", "dee", "yo", "ay", "baby", "girl", "boy", "hey", "hi", "bye", "ha", "ha-ha", "na-na-na", 
+    "sha-la-la", "hmm-mm", "ah-ah", "oh-oh", "da-da", "uh-oh", "yeah-yeah", "do-do", "woo", "y'all", "ain't", 
+    "gotta", "wanna", "gonna", "lemme", "yeah-yeah-yeah", "uh-huh", "uh-uh", "woah-woah"
+])
+
+# Text cleaning, stopword removal using custom stopwords
 def advanced_preprocess_lyrics(lyrics):
     lyrics = re.sub(r'[^\w\s]', '', lyrics)  # Remove punctuation
     lyrics = lyrics.lower()  # Convert to lowercase
     tokens = lyrics.split()  # Tokenize
-    tokens = [word for word in tokens if word not in stop_words]  # Remove stopwords
+    tokens = [word for word in tokens if word not in custom_stopwords]  # Remove custom stopwords
     return ' '.join(tokens)
 
 data['lyrics'] = data['lyrics'].apply(advanced_preprocess_lyrics)
@@ -63,26 +76,3 @@ data['artists'] = data['artists'].apply(advanced_preprocess_lyrics)
 
 # Save the cleaned data
 data.to_csv('../Datasets/cleaned_spotify_songs.csv', index=False)
-
-# List of words to exclude from the WordCloud
-exclude_list = ["not a good word list to show"]
-
-def remove_excluded_words(lyrics, exclude_list):
-    words = lyrics.split()
-    filtered = [word for word in words if word not in exclude_list]
-    return ' '.join(filtered)
-
-# Combine all lyrics, excluding words from the exclude list
-filtered_lyrics = ' '.join(
-    data['lyrics'].apply(lambda x: remove_excluded_words(x, exclude_list))
-)
-
-# Generate the WordCloud with filtered lyrics
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate(filtered_lyrics)
-
-# Plot the WordCloud
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title("Most Common Words in Song Lyrics (Filtered)")
-plt.show()
